9e1693388935a2be672ab86ad914de7a8369fdcf
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2025-05-02 09:46:37 +0700

    Fungsi bubbleSortArray() bagian awal (looping do-while)

--- file1.cpp
+++ file1.cpp
@@ -4,8 +4,7 @@ using namespace std;
 int element[10];
 int npanjang;
 int x;
-
-void input() {
+void input() { //prosedur input
     while (true){
         cout << "Masukkan banyaknya elemen pada array: ";
         cin >> npanjang;
@@ -16,14 +15,20 @@ void input() {
             cout << "\nAraay maksimal 20 elemen.\n";
         }
     }
-}
-cout << endl;
-cout << "=====================" << endl;
-cout << "Masukkan elemen array" << endl;
-cout << "=====================" << endl;
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen array" << endl;
+    cout << "=====================" << endl;
 
-for(int i=0;i<npanjang;i++){
-    cout << "Data ke-" << (i+1) << " = ";
-    cin >> element[i];
+    for(int i=0;i<npanjang;i++){
+        cout << "Data ke-" << (i+1) << " = ";
+        cin >> element[i];
+    }
 }
-
+void bubbleSortArray(){
+    int pass = 1; //step 1
+   do{
+    for (int j=0;j<=npanjang-1-pass;j++){ //step 2
+    }
+}
+}
