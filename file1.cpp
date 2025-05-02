6426a401361c523c84f884cbfe69b98752b75033
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2025-05-02 09:43:23 +0700

    Fungsi input() bagian pengisian array

--- file1.cpp
+++ file1.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int element[10];
 int npanjang;
-int x;\
+int x;
+
 void input() {
     while (true){
         cout << "Masukkan banyaknya elemen pada array: ";
@@ -15,3 +16,14 @@ void input() {
             cout << "\nAraay maksimal 20 elemen.\n";
         }
     }
+}
+cout << endl;
+cout << "=====================" << endl;
+cout << "Masukkan elemen array" << endl;
+cout << "=====================" << endl;
+
+for(int i=0;i<npanjang;i++){
+    cout << "Data ke-" << (i+1) << " = ";
+    cin >> element[i];
+}
+
